f6f1c59b1008be3bf14314207d9ade759e2ee519
Thomas Rodia <rodiathomas@itis-molinari.eu>
2025-02-10 12:45:28 +0100

    parte grafica statica

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const multer  = require('multer');
 //const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const path = require('path');
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "Public/Assets")));
 
 const server = http.createServer(app);
 server.listen(80, () => {
